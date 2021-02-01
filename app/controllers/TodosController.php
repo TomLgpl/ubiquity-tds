26e70d9eb9d01dc6624269cb6fd01f38db08a679
Tom <21903078@etu.unicaen.fr>
2021-02-01 14:33:58 +0100

    newList done

--- app/controllers/TodosController.php
+++ app/controllers/TodosController.php
@@ -1,5 +1,10 @@
 <?php
 
+/**
+ * Controlle TodosController
+ * @property \Ajax\php\ubiquity\JsUtils $jquery
+ */
+
 namespace controllers;
 use Ubiquity\attributes\items\router\Route;
 use Ubiquity\attributes\items\router\Post;
@@ -30,7 +35,8 @@ class TodosController extends ControllerBase{
             $list = USession::get(self::LIST_SESSION_KEY, []);
             return $this->display($list);
         }
-        $this->showMessage('Bienvenue !', 'TodoLists permet de générer des listes ...', 'info', 'info circle outline', [['url' => Router::path('todos.new'), 'caption' => 'Créer une nouvelle liste', 'style' => 'basic inverted']]);
+        $this->showMessage('Bienvenue !', 'TodoLists permet de générer des listes ...', 'info', 'info circle outline',
+            [['url' => Router::path('todos.new'), 'caption' => 'Créer une nouvelle liste', 'style' => 'basic inverted']]);
 	}
 
 	private function display(array $list){
@@ -63,8 +69,18 @@ class TodosController extends ControllerBase{
     }
 
     #[Get(path: "todos/new/{force}", name: "todos.new")]
-    public function newList($force){
-
+    public function newList($force = false){
+        if(USession::exists(self::LIST_SESSION_KEY) && $force == false){
+            $this->showMessage("Nouvelle Liste", "Une liste existe déjà. Voulez-vous la vider ?", "", "",
+                [['url' => Router::path('todos.new/1'), 'caption' => 'Créer une nouvelle liste', 'style' => 'basic inverted'],
+                ['url' => Router::path('todos.menu'), 'caption' => 'Annuler', 'style' => 'basic inverted']]);
+        }
+        else {
+            $this->showMessage("Nouvelle Liste", "Une nouvelle liste a été crée !", "", "check square outline");
+            $list = [];
+            USession::set(self::ACTIVE_LIST_SESSION_KEY, $list);
+            $this->display($list);
+        }
     }
 
     #[Get(path: "todos/saveList", name: "todos.save")]
