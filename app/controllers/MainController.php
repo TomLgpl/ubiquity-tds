0f31578bf989d194a4ba8dabb8fbe0a204a21736
TomLgpl <74899375+TomLgpl@users.noreply.github.com>
2021-03-22 22:07:17 +0100

    Promotions
    Affichage des promotions sur la page accueil

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -1,23 +1,25 @@
 <?php
 namespace controllers;
- use models\Order;
- use models\User;
- use Ubiquity\controllers\auth\AuthController;
- use Ubiquity\controllers\auth\WithAuthTrait;
- use Ubiquity\orm\DAO;
- use Ubiquity\utils\http\USession;
+use models\Order;
+use models\Product;
+use models\User;
+use Ubiquity\controllers\auth\AuthController;
+use Ubiquity\controllers\auth\WithAuthTrait;
+use Ubiquity\orm\DAO;
+use Ubiquity\utils\http\USession;
 
- /**
-  * Controller MainController
-  */
+/**
+ * Controller MainController
+ */
 class MainController extends ControllerBase{
 
     use WithAuthTrait;
 
-	public function index(){
+    public function index(){
         $user = DAO::getById(User::class, [USession::get('idUser')], ['orders', 'baskets', ]);
-		$this->loadView("MainController/index.html", ['user' => $user]);
-	}
+        $promotions = DAO::getAll(Product::class, 'promotion <> 0.00', ['section']);
+        $this->loadView("MainController/index.html", ['user' => $user, 'promotions' => $promotions]);
+    }
 
     protected function getAuthController(): AuthController {
         return new Authentification($this);
