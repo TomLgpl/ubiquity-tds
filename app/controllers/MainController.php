177e86c81427efff8360dc345b8d2f04eb198e66
tom Legoupil <21903078@etu.unicaen.fr>
2021-03-24 00:11:18 +0100

    store
    début ajout page store

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -1,8 +1,11 @@
 <?php
 namespace controllers;
+use models\Section;
+use Ubiquity\attributes\items\router\Get;
 use models\Order;
 use models\Product;
 use models\User;
+use Ubiquity\attributes\items\router\Route;
 use Ubiquity\controllers\auth\AuthController;
 use Ubiquity\controllers\auth\WithAuthTrait;
 use Ubiquity\orm\DAO;
@@ -15,6 +18,7 @@ class MainController extends ControllerBase{
 
     use WithAuthTrait;
 
+    #[Route(path:"_default", name:"home")]
     public function index(){
         $user = DAO::getById(User::class, [USession::get('idUser')], ['orders', 'baskets', ]);
         $promotions = DAO::getAll(Product::class, 'promotion <> 0.00', ['section']);
@@ -24,5 +28,12 @@ class MainController extends ControllerBase{
     protected function getAuthController(): AuthController {
         return new Authentification($this);
     }
+
+	#[Get(path: "store",name: "store")]
+	public function store(){
+        $sections = DAO::getAll(Section::class);
+        $promotions = DAO::getAll(Product::class, 'promotion <> 0.00', ['section']);
+		$this->loadView('MainController/store.html', ['sections' => $sections, 'promotions' => $promotions]);
+	}
 
 }
